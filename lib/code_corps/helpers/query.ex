f45d31e26be247640687c93bd4596ad7e8d078ab
Nikola Begedin <begedinnikola@gmail.com>
2016-10-14 14:36:20 +0200

    Task complete swithc to ja_resource, with caveat
    - cant set meta without explicitly rendering

--- lib/code_corps/helpers/query.ex
+++ lib/code_corps/helpers/query.ex
@@ -1,6 +1,6 @@
 defmodule CodeCorps.Helpers.Query do
   import CodeCorps.Helpers.String, only: [coalesce_id_string: 1, coalesce_string: 1]
-  import Ecto.Query, only: [where: 3, limit: 2]
+  import Ecto.Query, only: [where: 3, limit: 2, order_by: 2]
 
   def id_filter(query, id_list) do
     ids = id_list |> coalesce_id_string
@@ -29,6 +29,37 @@ defmodule CodeCorps.Helpers.Query do
   def title_filter(query, _), do: query
 
   # end skill queries
+
+  # task queries
+
+  def number_as_id_filter(query, %{"id" => number}) do
+    query |> where([object], object.number == ^number)
+  end
+  def number_as_id_filter(query, _), do: query
+
+  def project_filter(query, %{"project_id" => project_id}) do
+    query |> where([object], object.project_id == ^project_id)
+  end
+  def project_filter(query, _), do: query
+
+  def task_type_filter(query, %{"task_type" => task_type_list}) do
+    task_types = task_type_list |> coalesce_string
+    query |> where([object], object.task_type in ^task_types)
+  end
+  def task_type_filter(query, _), do: query
+
+  def task_status_filter(query, %{"status" => status}) do
+    query |> where([object], object.status == ^status)
+  end
+  def task_status_filter(query, _), do: query
+
+  # end task queries
+
+  # sorting
+
+  def sort_by_newest_first(query), do: query |> order_by([desc: :inserted_at])
+
+  # end sorting
 
   # finders
 
